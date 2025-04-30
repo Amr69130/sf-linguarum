02a381d87f5366c5f38f09f28b7db36111acd75b
Amr69130 <amrouchenorris@gmail.com>
2025-04-30 13:42:24 +0200

    fix: simplification du code pour l'affichage simple (boucle for avec if)

--- src/DataFixtures/LanguageFixtures.php
+++ src/DataFixtures/LanguageFixtures.php
@@ -10,30 +10,47 @@ class LanguageFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // Création des langues de base (par exemple, le Proto-Indo-Européen)
+
         $protoIndoEuropean = new Language();
         $protoIndoEuropean->setName('Proto-Indo-European');
         $protoIndoEuropean->setDescription('Langue mère des langues indo-européennes.');
-
         $manager->persist($protoIndoEuropean);
 
-        // Création du Latin, enfant du Proto-Indo-Européen
         $latin = new Language();
         $latin->setName('Latin');
         $latin->setDescription('Langue ancienne, ancêtre du français, espagnol, italien, etc.');
         $latin->setParent($protoIndoEuropean);
-
         $manager->persist($latin);
 
-        // Création du Français, enfant du Latin
         $french = new Language();
         $french->setName('Français');
         $french->setDescription('Langue romane parlée en France, Belgique, Suisse, etc.');
         $french->setParent($latin);
-
         $manager->persist($french);
 
-        // Sauvegarder les objets dans la base de données
+        $protoGermanic = new Language();
+        $protoGermanic->setName('Proto-Germanique');
+        $protoGermanic->setDescription('Langue ancestrale des langues germaniques (allemand, anglais, néerlandais, etc.)');
+        $manager->persist($protoGermanic);
+
+        $oldEnglish = new Language();
+        $oldEnglish->setName('Vieil Anglais');
+        $oldEnglish->setDescription("Ancêtre direct de l’anglais moderne.");
+        $oldEnglish->setParent($protoGermanic);
+        $manager->persist($oldEnglish);
+
+        $oldHighGerman = new Language();
+        $oldHighGerman->setName('Vieux Haut Allemand');
+        $oldHighGerman->setDescription("Ancêtre des dialectes allemands modernes.");
+        $oldHighGerman->setParent($protoGermanic);
+        $manager->persist($oldHighGerman);
+
+        $modernEnglish = new Language();
+        $modernEnglish->setName('Anglais Moderne');
+        $modernEnglish->setDescription("Langue germanique parlée principalement au Royaume-Uni, aux États-Unis, etc.");
+        $modernEnglish->setParent($oldEnglish);
+        $manager->persist($modernEnglish);
+
         $manager->flush();
     }
 }
